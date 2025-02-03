291c4343156fbf67fd56e7e75df8642202c5aada
JARIAS172a <che.arias77@gmail.com>
2025-02-03 13:28:18 -0600

    first upload

---
+++ Models/Data/CarritoCompras.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models.Data;
+
+public partial class CarritoCompras
+{
+    public int IdCarrito { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public DateTime? FechaCompra { get; set; }
+
+    public decimal Subtotal { get; set; }
+
+    public decimal Total { get; set; }
+
+    public string TipoPago { get; set; } = null!;
+
+    public virtual ICollection<DetalleCarrito> DetalleCarritos { get; set; } = new List<DetalleCarrito>();
+
+    public virtual Usuarios IdUsuarioNavigation { get; set; } = null!;
+}
---
+++ ProyectoVentaMusical/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Models.Data;
+using ProyectoVentaMusical.Data;
+
+namespace ProyectoVentaMusical
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("ConexionSQL") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{area=Usuarios}/{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
